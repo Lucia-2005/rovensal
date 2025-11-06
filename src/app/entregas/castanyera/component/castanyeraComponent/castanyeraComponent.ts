6ded379737b3080b1ce79d58a5b4d6acac05d824
Lucia Castanera <castaneralucia@gmail.com>
2025-11-06 12:33:15 +0100

    base de la página, api configurada, tarjetas creadas

--- src/app/entregas/castanyera/component/castanyeraComponent/castanyeraComponent.ts
+++ src/app/entregas/castanyera/component/castanyeraComponent/castanyeraComponent.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { Jsonplacecastanyera } from '../../services/jsonCastanyera';
+import { Castanyera } from '../../model/castanyeraInterface';
+import { CastanyeraCards } from '../castanyera-cards/castanyera-cards';
 
 @Component({
   selector: 'app-castanyera',
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, CastanyeraCards],
   templateUrl: './castanyeraComponent.html',
-  styleUrl: './castanyeraComponent.css',
-  standalone: true
+  styleUrls: ['./castanyeraComponent.css'],
+  standalone: true,
 })
-export class CastanyeraComponent {}
+export class CastanyeraComponent {
+  personajes: Castanyera[] = [];
+
+  constructor(private jsonCastanyera: Jsonplacecastanyera) {}
+
+  ngOnInit() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.jsonCastanyera.getAllCharacters().subscribe((characters: Castanyera[]) => {
+      this.personajes = characters;
+      console.log(this.personajes);
+    });
+  }
+}
--- src/app/entregas/castanyera/services/jsonCastanyera.ts
+++ src/app/entregas/castanyera/services/jsonCastanyera.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Castanyera } from '../model/castanyeraInterface';
- 
+
 @Injectable({
   providedIn: 'root',
 })
 export class Jsonplacecastanyera {
   constructor(private oHttpClient: HttpClient) {}
 
-  getAllAlbums(): Observable<Castanyera[]> {
-    return this.oHttpClient.get<Castanyera[]>('https://api.disneyapi.dev/character');
+  // The external API returns an object { info: {...}, data: [...] }
+  // Map the response to the `data` array so callers receive Castanyera[]
+  getAllCharacters(): Observable<Castanyera[]> {
+    return this.oHttpClient
+      .get<any>('https://api.disneyapi.dev/character')
+      .pipe(map((res) => res.data as Castanyera[]));
   }
 }
